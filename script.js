a3083c407a042d65c3240fe72e3c31fef9ac793a
Vijul Malviya <50556784+VijulMalviya@users.noreply.github.com>
2021-03-25 15:46:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+//  navigation bar
+ 
  const cross = document.getElementById('cross');
  const nav = document.getElementById('nav');
  const menu = document.getElementById('menu');
+
+
 
 
 menu.addEventListener('click', function(){
@@ -11,3 +15,35 @@ cross.addEventListener('click', function(){
     nav.style.transitionDuration ="1s";
     nav.style.marginLeft = "-50%";
 })
+
+
+// products image gallery
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
